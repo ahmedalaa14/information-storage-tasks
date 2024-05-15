25be6f543349c5abee5df6f22f2d2ae62d38262d
Ahmed Alaa <ahmedalaa24683901@gmail.com>
2024-05-15 04:00:28 +0300

    seekg function created

---
+++ task5.cpp
@@ -0,0 +1,64 @@
+#include<iostream>
+#include<fstream>
+#include<string>
+using namespace std;
+
+int main() {
+    char ch;
+    int pos;
+    ifstream file;
+    file.open("file.txt");
+
+    cout << file.tellg() << endl; // get position of pointer
+
+    // read char by char
+    while (!file.eof()) {
+        pos = file.tellg(); // get position of pointer
+        if (file.fail())
+            break;
+        file.get(ch); // read the character from the file
+        cout << "Char : " << ch << " ";
+        cout << "Position :" << pos << endl;
+    }
+
+    file.close();
+    return 0;
+}
+// get position (write)
+#include <iostream>
+#include <fstream>
+#include <string>
+using namespace std;
+int main(){
+    string str ;
+    int pos;
+    ofstream f;
+    f.open("file.txt");
+    cout << f.tellp()<< endl;
+    cout << "Enter data: ";
+    cin >> str;
+    f << str ;
+    pos = f.tellp();
+    cout << "Position: " << pos << endl;
+    f.close();
+    return 0;
+}
+
+/**************** seekg from begging(read) ***/
+#include<iostream>
+#include<fstream>
+#include<string>
+using namespace std;
+int main()
+{
+char ch;
+
+ifstream file;
+file.open("a.txt");
+file.seekg(4,ios::beg);
+file >> ch;
+cout << ch << endl;
+
+file.close();
+system("pause");
+}
