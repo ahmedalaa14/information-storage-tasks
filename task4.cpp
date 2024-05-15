d64e7d7622cb58f11bafdf44faa268cf3b96cf5b
Ahmed Alaa <ahmedalaa24683901@gmail.com>
2024-05-15 03:44:38 +0300

    task4 updated

--- task4.cpp
+++ task4.cpp
@@ -25,3 +25,77 @@ int main() {
 
     return 0;
 }
+
+// write char by char
+#include <iostream>
+#include <string>
+#include <fstream>
+using namespace std;
+int main(){
+    char ch;
+    ofstream f;
+    f.open("file.txt");
+    cout << "enter character data: ";
+    cin.get(ch);
+    while(ch != '.'){
+        f.put(ch);
+        cin.get(ch);
+    }
+    f.close();
+    return 0;
+}
+
+//copy file
+#include <iostream>
+#include <fstream>
+#include <string>
+using namespace std;
+int main(){
+    string data;
+    ifstream f1;
+    ofstream f2;
+    f1.open("file.txt", ios::in);
+    f2.open("file2.txt", ios::out);
+    while(getline(f1, data)){
+        f2 << data << endl;
+    }
+    f1.close();
+    f2.close();
+    return 0;
+}
+
+//copy text file using get , put 
+#include <iostream>
+#include <fstream>
+#include <string>
+using namespace std;
+int main(){
+    char ch;
+    ifstream f1;
+    ofstream f2;
+    f1.open("file.txt", ios::in);
+    f2.open("file2.txt", ios::out);
+    while(f1.get(ch)){
+        f2.put(ch);
+    }
+    f1.close();
+    f2.close();
+    return 0;
+}
+
+// fail fnction 
+#include <iostream>
+#include <fstream>
+#include <string>
+using namespace std;
+int main(){
+    string data;
+    ifstream f;
+    f.open("file.txt", ios::in);
+    if(f.fail()){
+        cout << "file not found" << endl;
+    }else{
+        cout << "success" << endl;
+    f.close();
+    return 0;
+}
